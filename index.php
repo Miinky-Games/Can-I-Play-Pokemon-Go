6a578182a8c69c5013f8354f967d8e7a4d2ed659
Nick Mudry <nick@mudry.me>
2016-07-17 18:11:55 -0400

    minor tweaks

--- index.php
+++ index.php
@@ -1,70 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Is Pokemon Go Down right now?">
-    <meta name="author" content="">
-
-    <title>Can I Play Pokemon Go - Is Pokemon Go Down Right Now?</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/scrolling-nav.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
+<?php 
+include_once("mysql/mysql.php"); 
+include_once("func.php");
+include_once("header.php");
+?>
 
 <!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
     <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Can I Play Pokemon Go</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <li class="hidden">
-                        <a class="page-scroll" href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#server-status">Server Status</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#release-0dates">Release Dates</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#stats">Pokemon Go Server Stats</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+    <?php include_once("nav.php"); ?>
 
     <!-- Intro Section -->
     <section id="intro" class="intro-section">
@@ -72,9 +17,13 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1>Yes, Pokemon Go Is Probably Down!</h1> <? //TODO: Make this say random things about pokemon go being down. ?>
-                    <p><strong>Wrong?</strong> Report server status by <a href="report.php">clicking here!</a>
-                    <a class="btn btn-default page-scroll" href="#about">Looking for more Pokemon?!</a>
+                    <p><strong>Wrong?</strong> Report server status by <a href="report.php">clicking here!</a></p>
+                    <p><a class="btn btn-default page-scroll" href="#release-dates">Looking for more Pokemon?!</a></p>
+
                 </div>
+            </div>
+            <div class="col-xs-12">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris metus lorem, accumsan a arcu eget, maximus eleifend orci. Nunc ut erat vitae urna aliquam pulvinar. Suspendisse facilisis turpis ut quam commodo aliquet. Curabitur ultrices scelerisque nunc, et commodo nunc. Nulla quam elit, vulputate vel odio ut, luctus lacinia dolor. Nunc luctus arcu in sem maximus egestas.  
             </div>
         </div>
     </section>
@@ -85,6 +34,9 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1>Server Status</h1>
+                    <p>42,069 people have reported Pokemon Go is down in their region.</p>
+                    <p>690 trainers have reported Pokemon Go is currently working in their region!</p>
+                    <p>Current majority status is... <strong>DOWN!</strong></p>
                 </div>
             </div>
         </div>
@@ -107,6 +59,9 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1>Site Information and Stats</h1>
+                    <p>42,069 people have reported Pokemon Go is down in their region.</p>
+                    <p>690 trainers have reported Pokemon Go is currently working in their region!</p>
+                    <p>Current majority status is... <strong>DOWN!</strong></p>
                 </div>
             </div>
         </div>
